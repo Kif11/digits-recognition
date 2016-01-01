082e2528a08fbbfc4a97bf2a3b109c4b1305e6de
Kirill Kovalevskiy <kovalewskiy@gmail.com>
2015-12-31 16:04:38 -0800

    Initial Commit

---
+++ digits_recognition/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  digits_recognition
+//
+//  Created by Amy on 12/31/15.
+//  Copyright (c) 2015 Kirill. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
